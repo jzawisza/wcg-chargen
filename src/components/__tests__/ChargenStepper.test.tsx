2139dd0950b9671e13104b6b21bdc6f3215eec5d
Jim Prakash-Zawisza <jpz@Jims-MacBook-Air.local>
2023-10-15 22:29:56 -0400

    Fix issues with ChargenStepper tests caused by use of React context

--- src/components/__tests__/ChargenStepper.test.tsx
+++ src/components/__tests__/ChargenStepper.test.tsx
@@ -1,11 +1,23 @@
 import ChargenStepper from "../ChargenStepper";
+import { NextButtonEnabledContext } from "../../Context";
 import { render, screen, act, waitFor, fireEvent } from '@testing-library/react';
 import userEvent from '@testing-library/user-event';
+import { useContext } from "react";
+
+const CustomComponent = () => {
+    const { setNextEnabled } = useContext(NextButtonEnabledContext);
+
+    return (
+        <button onClick={() => setNextEnabled(true)} aria-label="enableNext">
+            Enable Next
+        </button>
+    );
+};
 
 const testSteps = [
-    { title: 'First Step'},
-    { title: 'Second Step'},
-    { title: 'Third Step' }
+    { title: 'First Step', content: <CustomComponent /> },
+    { title: 'Second Step', content: <CustomComponent /> },
+    { title: 'Third Step', content: <CustomComponent /> }
 ];
 
 test('only next button and not previous button displayed on initial load', () => {
@@ -20,6 +32,10 @@ test('only next button and not previous button displayed on initial load', () =>
 
 test('after clicking next button, both previous and next buttons display', async () => {
     render(<ChargenStepper steps={testSteps} />);
+
+    // Trigger custom component to set context so that next button is enabled
+    const enableNextButton = screen.getByRole("button", { name: "enableNext" });
+    fireEvent.click(enableNextButton);
 
     const nextButton = screen.getByText("Next");
     act(() => {
@@ -37,9 +53,14 @@ test('after clicking next button, both previous and next buttons display', async
 test('only previous button and not next button displayed on final step', async () => {
     render(<ChargenStepper steps={testSteps} />);
 
+    const enableNextButton = screen.getByRole("button", { name: "enableNext" });
     const nextButton = screen.getByText("Next");
 
+    // Before triggering a click on Next, trigger the custom component so the
+    // context is updated to enable the Next button
+    fireEvent.click(enableNextButton);
     fireEvent.click(nextButton);
+    fireEvent.click(enableNextButton);
     fireEvent.click(nextButton);
     
     await waitFor(() => {
