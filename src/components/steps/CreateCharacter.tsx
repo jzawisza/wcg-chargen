68302d42ad09127ace277223da3bca9e22490ea9
Jim Prakash-Zawisza <jpz@Jims-MacBook-Air.local>
2023-10-21 22:27:47 -0400

    Add textbox for character name to Create Character component

--- src/components/steps/CreateCharacter.tsx
+++ src/components/steps/CreateCharacter.tsx
@@ -1,12 +1,17 @@
 import React, { MouseEvent, useState } from "react";
-import { Radio, RadioChangeEvent, Button, Result } from "antd";
+import { Radio, RadioChangeEvent, Button, Result, Input, Space } from "antd";
 
 const PDF_SHEET_TYPE = 'pdf';
 const GOOGLE_SHEETS_SHEET_TYPE='googlesheets';
 
 const CreateCharacter: React.FC = () => {
+    const [charName, setCharName] = useState('');
     const [charSheetType, setCharSheetType] = useState<string | null>(null);
     const [charGenerated, setCharGenerated] = useState(false);
+
+    const onInputCharacterName = (e: React.ChangeEvent<HTMLInputElement>) => {
+        setCharName(e.target.value);
+    };
 
     const onRadioGroupChange = (e: RadioChangeEvent) => {
         setCharSheetType(e.target.value);
@@ -36,24 +41,31 @@ const CreateCharacter: React.FC = () => {
     // Otherwise, show the base screen
     return (
         <div>
-            <p>Choose the type of character sheet you want to create, and then click on Create Character to generate the sheet.</p>
-            <p><b>TODO:</b> make this actually work</p>
-            <div className="charSheetButtonCenter">
-                <Radio.Group buttonStyle="solid" onChange={onRadioGroupChange} value={charSheetType}>
-                    <Radio.Button value={PDF_SHEET_TYPE}>PDF</Radio.Button>
-                    <Radio.Button value={GOOGLE_SHEETS_SHEET_TYPE}>Google Sheet</Radio.Button>
-                </Radio.Group>
-                {charSheetType === PDF_SHEET_TYPE && (
-                    <p className="charSheetDescription">Official OSF character sheet</p>
-                )}
-                {charSheetType === GOOGLE_SHEETS_SHEET_TYPE && (
-                    <p className="charSheetDescription">Homebrew character sheet with dynamic calculation of modifiers</p>
-                )}
-                {charSheetType !== null && (
-                    <div className="createCharButton">
-                        <Button type="primary" onClick={onCreateCharacter}>Create Character</Button>
-                    </div>
-                )}
+            <p>Now that you have designed your character, the last step is to give them a name.  Enter a name for your character.  Once you have done this, the buttons below will be activated.</p>
+            <Space>
+                <b>Character name:</b>
+                <Input onChange={onInputCharacterName}/>
+            </Space>
+            <div className="charSheetSelectorArea">
+            <p>Once these buttons are activated, choose the type of character sheet you want to create.  This will reveal the Create Character button.  Press this button to create your character.</p>
+            <p>Since this is just a mockup, <b>character creation is not working yet.</b>  Stay tuned...</p>
+                <div className="charSheetButtonCenter">
+                    <Radio.Group buttonStyle="solid" onChange={onRadioGroupChange} value={charSheetType} disabled={charName === ''}>
+                        <Radio.Button value={PDF_SHEET_TYPE}>PDF</Radio.Button>
+                        <Radio.Button value={GOOGLE_SHEETS_SHEET_TYPE}>Google Sheet</Radio.Button>
+                    </Radio.Group>
+                    {charSheetType === PDF_SHEET_TYPE && (
+                        <p className="charSheetDescription">Official OSF character sheet</p>
+                    )}
+                    {charSheetType === GOOGLE_SHEETS_SHEET_TYPE && (
+                        <p className="charSheetDescription">Homebrew character sheet with dynamic calculation of modifiers</p>
+                    )}
+                    {charSheetType !== null && (
+                        <div className="createCharButton">
+                            <Button type="primary" onClick={onCreateCharacter}>Create Character</Button>
+                        </div>
+                    )}
+                </div>
             </div>
         </div>
     );
--- src/components/steps/__tests__/CreateCharacter.test.tsx
+++ src/components/steps/__tests__/CreateCharacter.test.tsx
@@ -8,14 +8,34 @@ beforeAll(() => {
     });
 });
 
-test('initial load displays buttons for choosing character sheet type', () => {
+const PDF_BUTTON_TEXT = 'PDF';
+const GOOGLE_SHEET_BUTTON_TEXT = 'Google Sheet';
+const CREATE_CHARACTER_BUTTON_TEXT = 'Create Character';
+
+test('initial load displays buttons for choosing character sheet type, but in disabled state', () => {
     render(<CreateCharacter />);
 
-    const pdfButton = screen.getByText('PDF');
-    const googleSheetButton = screen.getByText('Google Sheet');
+    const pdfButton = screen.getByText(PDF_BUTTON_TEXT);
+    const googleSheetButton = screen.getByText(GOOGLE_SHEET_BUTTON_TEXT);
 
     expect(pdfButton).toBeTruthy();
     expect(googleSheetButton).toBeTruthy();
+
+    const pdfButtonParent = pdfButton.parentElement;
+    const googleSheetButtonParent = googleSheetButton.parentElement;
+
+    expect(pdfButtonParent).toHaveClass('ant-radio-button-wrapper-disabled');
+    expect(googleSheetButtonParent).toHaveClass('ant-radio-button-wrapper-disabled');
+});
+
+test('initial load displays character name input box and input box descriptive text', () => {
+    render(<CreateCharacter />);
+
+    const charNameText = screen.getByText('Character name:');
+    expect(charNameText).toBeTruthy();
+
+    const inputNode = screen.getByRole('textbox', {name: ''});
+    expect(inputNode).toBeTruthy();
 });
 
 test('initial load does not display Create Character button', () => {
@@ -24,14 +44,47 @@ test('initial load does not display Create Character button', () => {
     expect(() => screen.getByText('CreateCharacter')).toThrow();
 })
 
+test('entering text in character name input box enables character sheet type buttons', () => {
+    render(<CreateCharacter />);
+
+    const inputNode = screen.getByRole('textbox', {name: ''});
+    fireEvent.change(inputNode, {target: {value: 'MyCharacter'}});
+
+    const pdfButtonParent = screen.getByText(PDF_BUTTON_TEXT).parentElement;
+    const googleSheetButtonParent = screen.getByText(GOOGLE_SHEET_BUTTON_TEXT).parentElement;
+
+    expect(pdfButtonParent).not.toHaveClass('ant-radio-button-wrapper-disabled');
+    expect(googleSheetButtonParent).not.toHaveClass('ant-radio-button-wrapper-disabled');    
+});
+
+test('entering and then deleting text from character name input box disables character sheet type buttons', async () => {
+    render(<CreateCharacter />);
+
+    const inputNode = screen.getByRole('textbox', {name: ''});
+    fireEvent.change(inputNode, {target: {value: 'MyCharacter'}});
+    fireEvent.change(inputNode, {target: {value: ''}});
+
+    await waitFor(() => {
+        const pdfButtonParent = screen.getByText(PDF_BUTTON_TEXT).parentElement;
+        const googleSheetButtonParent = screen.getByText(GOOGLE_SHEET_BUTTON_TEXT).parentElement;
+
+        expect(pdfButtonParent).toHaveClass('ant-radio-button-wrapper-disabled');
+        expect(googleSheetButtonParent).toHaveClass('ant-radio-button-wrapper-disabled');
+    });
+});
+
 test('clicking on PDF button displays appropriate text and Create Character button', () => {
     render(<CreateCharacter />);
 
-    const pdfButton = screen.getByText('PDF');
+    // Enable PDF button
+    const inputNode = screen.getByRole('textbox', {name: ''});
+    fireEvent.change(inputNode, {target: {value: 'MyCharacter'}});
+
+    const pdfButton = screen.getByText(PDF_BUTTON_TEXT);
     fireEvent.click(pdfButton);
 
     const pdfText = screen.getByText('Official OSF character sheet');
-    const createCharButton = screen.getByText('Create Character');
+    const createCharButton = screen.getByText(CREATE_CHARACTER_BUTTON_TEXT);
 
     expect(pdfText).toBeTruthy();
     expect(createCharButton).toBeTruthy();
@@ -40,11 +93,15 @@ test('clicking on PDF button displays appropriate text and Create Character butt
 test('clicking on Google Sheet button displays appropriate text and Create Character button', () => {
     render(<CreateCharacter />);
 
-    const googleSheetButton = screen.getByText('Google Sheet');
+    // Enable Google Sheet button
+    const inputNode = screen.getByRole('textbox', {name: ''});
+    fireEvent.change(inputNode, {target: {value: 'MyCharacter'}});
+
+    const googleSheetButton = screen.getByText(GOOGLE_SHEET_BUTTON_TEXT);
     fireEvent.click(googleSheetButton);
 
     const googleSheetText = screen.getByText('Homebrew character sheet with dynamic calculation of modifiers');
-    const createCharButton = screen.getByText('Create Character');
+    const createCharButton = screen.getByText(CREATE_CHARACTER_BUTTON_TEXT);
 
     expect(googleSheetText).toBeTruthy();
     expect(createCharButton).toBeTruthy();
@@ -53,10 +110,14 @@ test('clicking on Google Sheet button displays appropriate text and Create Chara
 test('clicking Create Character button displays appropriate screen', async () => {
     render(<CreateCharacter />);
 
-    const pdfButton = screen.getByText('PDF');
+    // Enable PDF button
+    const inputNode = screen.getByRole('textbox', {name: ''});
+    fireEvent.change(inputNode, {target: {value: 'MyCharacter'}});
+
+    const pdfButton = screen.getByText(PDF_BUTTON_TEXT);
     fireEvent.click(pdfButton);
 
-    const createCharButton = screen.getByText('Create Character');
+    const createCharButton = screen.getByText(CREATE_CHARACTER_BUTTON_TEXT);
     fireEvent.click(createCharButton);
 
     await waitFor(() => {
@@ -71,10 +132,14 @@ test('clicking Create Character button displays appropriate screen', async () =>
 test('clicking Create Another button reloads window', async () => {
     render(<CreateCharacter />);
 
-    const pdfButton = screen.getByText('PDF');
+    // Enable PDF button
+    const inputNode = screen.getByRole('textbox', {name: ''});
+    fireEvent.change(inputNode, {target: {value: 'MyCharacter'}});
+
+    const pdfButton = screen.getByText(PDF_BUTTON_TEXT);
     fireEvent.click(pdfButton);
 
-    const createCharButton = screen.getByText('Create Character');
+    const createCharButton = screen.getByText(CREATE_CHARACTER_BUTTON_TEXT);
     fireEvent.click(createCharButton);
 
     await waitFor(() => {
